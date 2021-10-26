83af5d372254c20804c8fc6ba1c95b9d210a93aa
brian724 <1348096958@qq.com>
2021-10-26 13:14:17 +0800

    first commit

---
+++ src/test/java/com/brian/tacocloud/TacoCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brian.tacocloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
